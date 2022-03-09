3bb1fed10fe37ccf35f1332e841e7546fba033af
khangbeo <khangbeo2012@gmail.com>
2022-03-09 14:27:41 -0800

    2 files modified

--- src/CloseIcon.js
+++ src/CloseIcon.js
@@ -4,8 +4,8 @@ export default function CloseIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="16"
+      height="16"
       fill="currentColor"
       className="bi bi-x-lg"
       viewBox="0 0 16 16"
